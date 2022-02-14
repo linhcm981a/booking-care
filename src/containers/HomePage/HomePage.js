f408ead0ca98f435a0c5a7cc7f1c4e3437eda8de
linhcm981a <linhcm981a@gmail.com>
2022-02-14 23:20:05 +0700

    custom-slick-49

---
+++ src/containers/HomePage/HomePage.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import HomeHeader from "./HomeHeader";
+import Specialty from "../HomePage/Section/Specialty";
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <HomeHeader />
+        <Specialty />
+        <div style={{ height: "300px" }}></div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
---
+++ src/containers/System/ModalUser.js
@@ -0,0 +1,169 @@
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { connect } from "react-redux";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import { emitter } from "../../utils/emitter";
+
+class ModalUser extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      firstName: "",
+      lastName: "",
+      address: "",
+    };
+    this.listenToEmitter();
+  }
+  listenToEmitter() {
+    emitter.on("EVENT_CLEAR_MODAL_DATA", () => {
+      this.setState({
+        email: "",
+        password: "",
+        firstName: "",
+        lastName: "",
+        address: "",
+      });
+    });
+  }
+
+  componentDidMount() {}
+
+  toggle = () => {
+    this.props.toggleFromParent();
+  };
+
+  handleOnChangeInput = (event, id) => {
+    let copyState = { ...this.state };
+    copyState[id] = event.target.value;
+    this.setState({
+      ...copyState,
+    });
+  };
+
+  checkValideInput = () => {
+    let isValid = true;
+    let arrInput = ["email", "password", "firstName", "lastName", "address"];
+    for (let i = 0; i < arrInput.length; i++) {
+      if (!this.state[arrInput[i]]) {
+        isValid = false;
+        alert("Missing required parameter" + arrInput[i]);
+        break;
+      }
+    }
+    return isValid;
+  };
+  handleAddNewUser = () => {
+    let isValid = this.checkValideInput();
+    if (isValid === true) {
+      this.props.createNewuser(this.state, "abc");
+    }
+  };
+
+  render() {
+    return (
+      <Modal
+        isOpen={this.props.isOpen}
+        toggle={() => {
+          this.toggle();
+        }}
+        className={"modal-user-container"}
+        size="lg"
+        centered
+      >
+        <ModalHeader
+          toggle={() => {
+            this.toggle();
+          }}
+        >
+          Create New User
+        </ModalHeader>
+        <ModalBody>
+          <div className="modal-user-body">
+            <div className="input-container">
+              <label>Email</label>
+              <input
+                type="text"
+                onChange={(event) => {
+                  this.handleOnChangeInput(event, "email");
+                }}
+                value={this.state.email}
+              />
+            </div>
+            <div className="input-container">
+              <label>Password</label>
+              <input
+                type="password"
+                onChange={(event) => {
+                  this.handleOnChangeInput(event, "password");
+                }}
+                value={this.state.password}
+              />
+            </div>
+            <div className="input-container">
+              <label>First name </label>
+              <input
+                type="text"
+                onChange={(event) => {
+                  this.handleOnChangeInput(event, "firstName");
+                }}
+                value={this.state.firstName}
+              />
+            </div>
+            <div className="input-container">
+              <label>Last Name</label>
+              <input
+                type="text"
+                onChange={(event) => {
+                  this.handleOnChangeInput(event, "lastName");
+                }}
+                value={this.state.lastName}
+              />
+            </div>
+            <div className="input-container max-width-input">
+              <label>Address</label>
+              <input
+                type="text"
+                onChange={(event) => {
+                  this.handleOnChangeInput(event, "address");
+                }}
+                value={this.state.address}
+              />
+            </div>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            color="primary"
+            onClick={() => {
+              this.handleAddNewUser();
+            }}
+            className="px-3"
+          >
+            Save
+          </Button>{" "}
+          <Button
+            color="secondary"
+            onClick={() => {
+              this.toggle();
+            }}
+            className="px-3"
+          >
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ModalUser);
